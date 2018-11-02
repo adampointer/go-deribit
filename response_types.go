33180473ba79a0ffc82c3b40ec96367735ceadc8
Adam Pointer <adam.pointer@skybettingandgaming.com>
2018-11-02 18:41:41 +0000

    Fix some small issues where unexpected types can be returned some edge cases

--- response_types.go
+++ response_types.go
@@ -4,21 +4,21 @@ import "encoding/json"
 
 // TradeResponse is the data returned by a trade event notification
 type TradeResponse struct {
-	TradeID       int     `json:"tradeId"`
-	Timestamp     int64   `json:"timeStamp"`
-	Instrument    string  `json:"instrument"`
-	Quantity      int     `json:"quantity"`
-	Price         float64 `json:"price"`
-	State         string  `json:"state"`
-	Direction     string  `json:"direction"`
-	OrderID       int     `json:"orderId"`
-	MatchingID    int     `json:"matchingId"`
-	MakerComm     float64 `json:"makerComm"`
-	TakerComm     float64 `json:"takerComm"`
-	IndexPrice    float64 `json:"indexPrice"`
-	Label         string  `json:"label"`
-	Me            string  `json:"me"`
-	TickDirection int     `json:"tickDirection"`
+	TradeID       int         `json:"tradeId"`
+	Timestamp     int64       `json:"timeStamp"`
+	Instrument    string      `json:"instrument"`
+	Quantity      json.Number `json:"quantity"`
+	Price         float64     `json:"price"`
+	State         string      `json:"state"`
+	Direction     string      `json:"direction"`
+	OrderID       int         `json:"orderId"`
+	MatchingID    int         `json:"matchingId"`
+	MakerComm     float64     `json:"makerComm"`
+	TakerComm     float64     `json:"takerComm"`
+	IndexPrice    float64     `json:"indexPrice"`
+	Label         string      `json:"label"`
+	Me            string      `json:"me"`
+	TickDirection int         `json:"tickDirection"`
 }
 
 // OrderBookResponse is the data returned by an orderbook change
@@ -37,9 +37,9 @@ type OrderBookResponse struct {
 
 // OrderBookEntry is an entry in the orderbook
 type OrderBookEntry struct {
-	Quantity int64   `json:"quantity"`
-	Price    float64 `json:"price"`
-	Cm       int64   `json:"cm"`
+	Quantity json.Number `json:"quantity"`
+	Price    float64     `json:"price"`
+	Cm       int64       `json:"cm"`
 }
 
 // OrderResponse is a response to an OrderRequest
@@ -51,12 +51,12 @@ type OrderResponse struct {
 
 // OrderResponseTrades trades is populated when a trade is executed immediately
 type OrderResponseTrades struct {
-	Label      string  `json:"label"`
-	SelfTrade  bool    `json:"selfTrade"`
-	Quantity   int     `json:"quantity"`
-	Price      float64 `json:"price"`
-	TradeSeq   int     `json:"tradeSeq"`
-	MatchingID string  `json:matchingId`
+	Label      string      `json:"label"`
+	SelfTrade  bool        `json:"selfTrade"`
+	Quantity   int         `json:"quantity"`
+	Price      float64     `json:"price"`
+	TradeSeq   int         `json:"tradeSeq"`
+	MatchingID json.Number `json:matchingId`
 }
 
 // OrderResponseDetail is the full details of the order
